3cdc5d05161580c16609b6a6be452d3fdf7e769b
Wizock <36351054+Wizock@users.noreply.github.com>
2021-02-16 23:06:57 +1000

    Add files via upload

---
+++ api.py
@@ -0,0 +1,143 @@
+import sqlite3
+from sqlite3 import Error
+import os
+
+#NOTE this function creates a admin account straight off the bat since it is required by the application to have a default admin account to gain functionality 
+
+def admin_register():
+    con = connect()
+    try:
+        #NOTE enters default credentials 
+        con.execute("INSERT INTO Admin (email,userName,password) VALUES (?,?,?)",("Wizock.Admin@mail.com","admin","admin"))
+        #NOTE the default username and password is admin admin 
+        con.commit()
+        con.close()
+        print("your Admin account has been created :)")
+    except Error as e:
+        print(e)
+def createTableFormat():
+    conn1 = connect()
+    try:
+        conn1.execute('''
+            CREATE TABLE Admin (
+                UserId INTEGER PRIMARY KEY NOT NULL,
+                email varchar(255),
+                userName varchar(255),
+                password varchar(255)
+                )
+                ''')
+        conn1.execute('''
+            CREATE TABLE employee_personal (
+                UserId INTEGER PRIMARY KEY NOT NULL,
+                firstName varchar(255),
+                lastName varchar(255),
+                email varchar(255),
+                userName varchar(255),
+                position varchar(255) ,
+                gender varchar(255),
+                age varchar(255),
+                adress varchar(255),
+                phonenumber int,
+                FOREIGN KEY (Email) REFERENCES employee_personal(normal_contact)
+                FOREIGN KEY (phonenumber) REFERENCES emergency_contact(phoneNo)
+                )
+            ''')
+        
+        conn1.execute(
+            '''
+            CREATE TABLE application_infomation(
+                UserId int,
+                Username varchar(255) PRIMARY KEY NOT NULL,
+                password varchar(255) UNIQUE,
+                email varchar(255),
+                FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
+                FOREIGN KEY (Email) REFERENCES employee_personal(normal_contact)
+                )
+            ''')
+            
+        conn1.execute(
+            '''
+            CREATE TABLE employee_pay(
+                UserId int,
+                Username varchar(255),
+                weeklyPay money PRIMARY KEY NOT NULL,
+                wage int UNIQUE,
+                age varchar(255),
+                FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
+                FOREIGN KEY (Username) REFERENCES application_infomation(employee_shifts)
+            )
+            '''
+        )
+        conn1.execute(
+            '''
+            CREATE TABLE employee_shifts(
+                UserId int,
+                Positions string PRIMARY KEY NOT NULL,
+                startingTime time,
+                endingTime time,
+                shiftsPerWeek int,
+                FOREIGN KEY (UserId) REFERENCES employee_personal(UserId)
+                )
+            ''')
+
+        conn1.execute(
+            '''
+            CREATE TABLE late_arrivals(
+                UserId int,
+                employeeName varchar(255),
+                startingTime time,
+                arrivalTime time,
+                lateBy time,
+                lateAmt int PRIMARY KEY NOT NULL ,
+                FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
+                FOREIGN KEY (employeeName) REFERENCES employee_personal(firstName)
+            )''')
+
+        conn1.execute(
+            '''
+            CREATE TABLE emergency_contact(
+                UserId int,
+                phoneNo int PRIMARY KEY NOT NULL,
+                Email string,
+                FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
+                FOREIGN KEY (Email) REFERENCES employee_personal(normal_contact)
+				)
+            '''
+        )
+
+        conn1.execute(
+            '''
+            CREATE TABLE normal_contact(
+                UserId int,
+                Email string PRIMARY KEY NOT NULL,
+                adress string ,
+                FOREIGN KEY (UserId) REFERENCES employee_personal(UserId)
+            )
+            '''
+        )
+        # this is a query which creates a table, this specific query sets up the table's format
+        check_Admin = conn1.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Admin'")
+        check_Workers = conn1.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='workers'")
+
+        if check_Admin != None:
+            conn1.execute("DROP TABLE Admin")
+        if check_Workers != None:
+            conn1.execute("DROP TABLE workers")
+    except Error as e:
+        print(e)
+
+#NOTE this function connects to the database file
+def connect():
+    Path = os.getcwd()+"\main_DataBase.db"
+    if Path != os.path.islink(Path):
+        con = sqlite3.connect(Path)
+#NOTE this function returns the connection variable so that it can be used anywhere with any variable name.
+    return con
+
+#NOTE this function checks if the database file exists or not since it causes errors if the application is asked to create a database that allready exists. 
+def checkIfEmpty():
+    Path = os.getcwd()+"\main_DataBase.db"
+    if os.path.exists(Path) == True:
+        return False
+    else:
+        return True
---
+++ main.py
@@ -0,0 +1,74 @@
+from api import *
+from session import * 
+import sqlite3
+
+#NOTE this function is a login system for admins. admins and workers need separate login systems since they have different tables. 
+def login_attemp_admin():
+    global username, password 
+
+    #NOTE connects to the database  
+    con = connect()
+    #NOTE starts a while loop as to give the user multiple chances to login however, it can be bruteforced.
+    while True:
+        #NOTE user enters their credentials 
+        username = input("Enter username:")
+        password = input("Enter password:")
+        cursor = con.cursor()
+        #NOTE runs a query to check if the entered credentials exist in the table or not. 
+        find_user_fromAdmin = cursor.execute(("SELECT * FROM Admin WHERE userName = ? AND password = ?"),(username,password))
+        resultsPassword = cursor.fetchall()
+        #NOTE if the login in is successful, then it starts the admin function.
+        if resultsPassword:
+            print("Welcome :" + username)
+            beginAdmin()
+        else:
+            #NOTE else it restarts the loop and prompts the user to enter their credentials correctly 
+            print("Username or Password not found")
+            print("try again")
+
+def login_attemp_workers():   
+    con = connect()
+    while True:
+        username = input("Enter username:")
+        password = input("Enter password:")
+        cursor = con.cursor()
+        find_user_fromAdmin = cursor.execute(("SELECT * FROM workers WHERE userName = ? AND password = ?"), (username, password))
+        firstName = cursor.execute("SELECT firstName FROM workers WHERE userName = ? ",(username,))
+        resultsPassword = cursor.fetchall()
+        if resultsPassword:
+            print("Welcome :",list(firstName))
+            break
+        print("Username or Password not found")
+        print("try again")
+#NOTE  this is the main function, its what binds the files together. 
+def main():
+    #NOTE it checks if a database file exists or not
+    permission = checkIfEmpty()
+
+    #NOTE if the file exists then it wont create the database file and it would just connect
+    if permission == True:
+        con = connect()
+        createTableFormat()
+        admin_register()
+
+    #NOTE if the file doesnt exist then it will create one
+    if permission == False:
+        con = sqlite3.connect('main_Database.db')
+        createTableFormat()
+        admin_register()
+
+    #NOTE this assure that the application is giving the correct privileges to the correct person 
+    adminOrNot = input("are you a worker or a admin?| type 'admin' or 'worker': ")
+
+    if adminOrNot == "admin":
+        print("please Login:\n")
+        login_attemp_admin()
+        
+    if adminOrNot == 'worker':
+        print("please Login:\n")
+        login_attemp_workers()  
+
+if __name__=="__main__":
+    main()
+
+
