e47aacbe93a2abc0b1bdef8f2708471cd1133857
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:18:51 +1000

    Update api.py

--- api.py
+++ api.py
@@ -2,31 +2,40 @@ import sqlite3
 from sqlite3 import Error
 import os
 
-#NOTE this function creates a admin account straight off the bat since it is required by the application to have a default admin account to gain functionality 
+# NOTE this function creates a admin account straight off the bat since it is required by the application to have a default admin account to gain functionality
+
 
 def admin_register():
     con = connect()
     try:
-        #NOTE enters default credentials 
-        con.execute("INSERT INTO Admin (email,userName,password) VALUES (?,?,?)",("Wizock.Admin@mail.com","admin","admin"))
-        #NOTE the default username and password is admin admin 
+        # NOTE enters default credentials
+        con.execute(
+            "INSERT INTO Admin (email,userName,password) VALUES (?,?,?)",
+            ("Wizock.Admin@mail.com", "admin", "admin"),
+        )
+        # NOTE the default username and password is admin admin
         con.commit()
         con.close()
         print("your Admin account has been created :)")
     except Error as e:
         print(e)
+
+
 def createTableFormat():
     conn1 = connect()
     try:
-        conn1.execute('''
+        conn1.execute(
+            """
             CREATE TABLE Admin (
                 UserId INTEGER PRIMARY KEY NOT NULL,
                 email varchar(255),
                 userName varchar(255),
                 password varchar(255)
                 )
-                ''')
-        conn1.execute('''
+                """
+        )
+        conn1.execute(
+            """
             CREATE TABLE employee_personal (
                 UserId INTEGER PRIMARY KEY NOT NULL,
                 firstName varchar(255),
@@ -41,10 +50,11 @@ def createTableFormat():
                 FOREIGN KEY (Email) REFERENCES employee_personal(normal_contact)
                 FOREIGN KEY (phonenumber) REFERENCES emergency_contact(phoneNo)
                 )
-            ''')
-        
+            """
+        )
+
         conn1.execute(
-            '''
+            """
             CREATE TABLE application_infomation(
                 UserId int,
                 Username varchar(255) PRIMARY KEY NOT NULL,
@@ -53,10 +63,11 @@ def createTableFormat():
                 FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
                 FOREIGN KEY (Email) REFERENCES employee_personal(normal_contact)
                 )
-            ''')
-            
+            """
+        )
+
         conn1.execute(
-            '''
+            """
             CREATE TABLE employee_pay(
                 UserId int,
                 Username varchar(255),
@@ -66,10 +77,10 @@ def createTableFormat():
                 FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
                 FOREIGN KEY (Username) REFERENCES application_infomation(employee_shifts)
             )
-            '''
+            """
         )
         conn1.execute(
-            '''
+            """
             CREATE TABLE employee_shifts(
                 UserId int,
                 Positions string PRIMARY KEY NOT NULL,
@@ -78,10 +89,11 @@ def createTableFormat():
                 shiftsPerWeek int,
                 FOREIGN KEY (UserId) REFERENCES employee_personal(UserId)
                 )
-            ''')
+            """
+        )
 
         conn1.execute(
-            '''
+            """
             CREATE TABLE late_arrivals(
                 UserId int,
                 employeeName varchar(255),
@@ -91,10 +103,11 @@ def createTableFormat():
                 lateAmt int PRIMARY KEY NOT NULL ,
                 FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
                 FOREIGN KEY (employeeName) REFERENCES employee_personal(firstName)
-            )''')
+            )"""
+        )
 
         conn1.execute(
-            '''
+            """
             CREATE TABLE emergency_contact(
                 UserId int,
                 phoneNo int PRIMARY KEY NOT NULL,
@@ -102,22 +115,26 @@ def createTableFormat():
                 FOREIGN KEY (UserId) REFERENCES employee_personal(UserId),
                 FOREIGN KEY (Email) REFERENCES employee_personal(normal_contact)
 				)
-            '''
+            """
         )
 
         conn1.execute(
-            '''
+            """
             CREATE TABLE normal_contact(
                 UserId int,
                 Email string PRIMARY KEY NOT NULL,
                 adress string ,
                 FOREIGN KEY (UserId) REFERENCES employee_personal(UserId)
             )
-            '''
+            """
         )
         # this is a query which creates a table, this specific query sets up the table's format
-        check_Admin = conn1.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Admin'")
-        check_Workers = conn1.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='workers'")
+        check_Admin = conn1.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='Admin'"
+        )
+        check_Workers = conn1.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='workers'"
+        )
 
         if check_Admin != None:
             conn1.execute("DROP TABLE Admin")
@@ -126,17 +143,19 @@ def createTableFormat():
     except Error as e:
         print(e)
 
-#NOTE this function connects to the database file
+
+# NOTE this function connects to the database file
 def connect():
-    Path = os.getcwd()+"\main_DataBase.db"
+    Path = os.getcwd() + "\main_DataBase.db"
     if Path != os.path.islink(Path):
         con = sqlite3.connect(Path)
-#NOTE this function returns the connection variable so that it can be used anywhere with any variable name.
+    # NOTE this function returns the connection variable so that it can be used anywhere with any variable name.
     return con
 
-#NOTE this function checks if the database file exists or not since it causes errors if the application is asked to create a database that allready exists. 
+
+# NOTE this function checks if the database file exists or not since it causes errors if the application is asked to create a database that allready exists.
 def checkIfEmpty():
-    Path = os.getcwd()+"\main_DataBase.db"
+    Path = os.getcwd() + "\main_DataBase.db"
     if os.path.exists(Path) == True:
         return False
     else:
